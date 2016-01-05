9b67e7d7e4bd725ad8367c3d28fc5dfc7f8431a0
Serg <sergikv@rambler.ru>
2016-01-06 00:55:29 +0300

    Первый образ

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Таблица сведений</title>
+  <meta charset="utf-8">
+  <link type="text/css" rel="stylesheet" href="delta.css" media="all" />
+</head>
+
+<body>
+	<div class="general">
+		<?php
+			include ("table-registr-View.php");
+		?>
+    </div> <!-- Конец класса General -->
+</body>
+
+</html>
