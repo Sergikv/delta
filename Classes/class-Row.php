8a935c1eb010995428da7163ff8e02cf69e1ac96
Serg <sergikv@rambler.ru>
2016-01-10 04:01:02 +0300

    Изменение name на text

--- Classes/class-Row.php
+++ Classes/class-Row.php
@@ -7,14 +7,15 @@ class Row {
 
 	function __construct ($row_array) {
 		$this -> row_array = $row_array;
-		$this -> end_array = count($row_array, COUNT_RECURSIVE) / 4 - 2;
+		$this -> end_array = count($row_array[text]) - 1;
+//		count($row_array, COUNT_RECURSIVE) / 4 - 2;
 	}
 
 	// Функция вывода строки
 	function view_row() {
 		echo "<tr>";
 		for ($i = 0; $i <= $this -> end_array; $i++)  {
-			$cell_table = new Cell ($this -> row_array[name][$i], $this -> row_array[col][$i], $this -> row_array[row][$i], $this -> row_array[font][$i]);
+			$cell_table = new Cell ($this -> row_array[text][$i], $this -> row_array[col][$i], $this -> row_array[row][$i], $this -> row_array[font][$i]);
 		 	$cell_table -> view_cell();
 		}
 		echo "</tr>";
